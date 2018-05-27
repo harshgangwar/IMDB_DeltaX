3490963e491326fcd27ee0f046ae287bc5ad67e5
harshgangwar <harshgangwar.rc@gmail.com>
2018-05-27 13:36:27 +0530

    added sql query

---
+++ sql_scripts.sql
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+CREATE TABLE Actors (
+
+    Id int IDENTITY(1,1) PRIMARY KEY,
+
+    Name varchar(255) NOT NULL,
+
+    Sex varchar(255) NOT NULL,
+
+    DOB date NOT NULL,
+
+	Bio varchar(255) NOT NULL
+
+);
+
+
+
